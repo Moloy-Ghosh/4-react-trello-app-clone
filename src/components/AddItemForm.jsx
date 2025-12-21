804e2208e13139175ef5a86e9a06dc4d44d4d43a
Moloy Ghosh <moloy.ghosh.cse@gmail.com>
2025-12-22 00:44:55 +0600

    third day second commit

---
+++ src/components/AddItemForm.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const AddItemForm=({listForm, submitHandler, title, onChangeHandler,setEditMode})=>{
+   return(
+      <div className="form-container">
+         <div className="form-card">
+            <form>
+               <textarea value={title} onChange={onChangeHandler} cols="30" row="2" className="form-textarea"></textarea>
+            </form>
+         </div>
+         <div>
+            <button className="add-button" onClick={submitHandler}>
+               {listForm ? "Add List" : "update Task"}
+            </button>
+            <p className="add-item-icon" onClick={()=>setEditMode(false)}>x</p>
+         </div>
+      </div>
+   )
+}
+
+export default AddItemForm
--- src/pages/BoardDetails.jsx
+++ src/pages/BoardDetails.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import {useState, useContext} from "react";
+import {Link , useParams} from "react-router-dom";
+import AddItem from "../components/AddItem";
+import AddItemForm from "../components/AddItemForm";
 
 const BoardDetails=()=>{
-   return<div>BoardDetails</div>
-}
+   return(
+      <div className="d-flex m-top-sm flex-direction-row">
+         <Link to="/">Back to Boards</Link>
+      </div>
+   );
+};
 
 export default BoardDetails;
